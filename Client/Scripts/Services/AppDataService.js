05a0c387d931d5b5702285dffd5e715c17b25f86
Unknown <Sapir.vishna@gmail.com>
2017-09-27 17:33:16 +0300

    add appData service

---
+++ Client/Scripts/Services/AppDataService.js
@@ -0,0 +1,26 @@
+/**
+ * Created by hack on 27/09/2017.
+ */
+app.factory("appData", ["$q", "$http", function ($q, $http) {
+    var appData = {};
+
+    appData.getAllDAta = function () {
+        var deferred = $q.defer();
+        $http.get("http://10.10.247.136:8080/data")
+            .then(function (data) {
+                deferred.resolve(data);
+            });
+        return deferred.promise;
+    };
+
+    appData.getById = function (id) {
+        var deferred = $q.defer();
+        $http.get("http://10.10.247.136:8080/dataById?id=" + id)
+            .then(function (nodeData) {
+                deferred.resolve(nodeData.data);
+            });
+        return deferred.promise;
+    }
+
+    return appData;
+}]);
--- Client/Scripts/UnitTree/UnitTreeController.js
+++ Client/Scripts/UnitTree/UnitTreeController.js
@@ -1,10 +1,10 @@
 /**
  * Created by hack on 27/09/2017.
  */
-app.controller("UnitTreeController", ["$http", function ($http) {
+app.controller("UnitTreeController", ["$http", "appData", function ($http, appData) {
     var self = this;
 
-    $http.get("http://10.10.247.136:8080/data")
+    appData.getAllDAta()
         .then(function (data) {
             var options = {
                 layout: {
@@ -28,16 +28,50 @@ app.controller("UnitTreeController", ["$http", function ($http) {
                 if (params.nodes.length === 1) {
                     var node = params.nodes[0];
 
-                    $http.get("http://10.10.247.136:8080/data?id=" + node)
+                    appData.getById(node)
                         .then(function (nodeData) {
-                            console.log(nodeData.data.name);
-                        }).catch(function () {
-
-                    });
+                            console.log(nodeData);
+                        })
+                        .catch();
                 }
             });
         })
-        .catch(function () {
+        .catch();
 
-        })
+    // $http.get("http://10.10.247.136:8080/data")
+    //     .then(function (data) {
+    //         var options = {
+    //             layout: {
+    //                 hierarchical: true
+    //             }
+    //         }
+    //
+    //         // create a network
+    //         var container = document.getElementById('unitTree');
+    //
+    //         // provide the data in the vis format
+    //         var tree = {
+    //             nodes: new vis.DataSet(data.data.nodes),
+    //             edges: new vis.DataSet(data.data.edges)
+    //         }
+    //
+    //         // initialize your network!
+    //         var network = new vis.Network(container, tree, options);
+    //
+    //         network.on("selectNode", function (params) {
+    //             if (params.nodes.length === 1) {
+    //                 var node = params.nodes[0];
+    //
+    //                 $http.get("http://10.10.247.136:8080/data?id=" + node)
+    //                     .then(function (nodeData) {
+    //                         console.log(nodeData.data.name);
+    //                     }).catch(function () {
+    //
+    //                 });
+    //             }
+    //         });
+    //     })
+    //     .catch(function () {
+    //
+    //     })
 }]);
