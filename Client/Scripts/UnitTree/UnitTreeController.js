3b48f69e9ee1dffbfde9ee4f1761dc138d4a8867
Unknown <liorsapir57@gmail.com>
2017-09-27 16:00:53 +0300

    Auto stash before merge of "master" and "origin/master"
    Get element by Id from backend

--- Client/Scripts/UnitTree/UnitTreeController.js
+++ Client/Scripts/UnitTree/UnitTreeController.js
@@ -1,14 +1,13 @@
 /**
  * Created by hack on 27/09/2017.
  */
-app.controller("UnitTreeController", [function () {
-    $.ajax({
-        method: "get",
-        url: "http://localhost:8080/data",
-        success: function (data) {
+app.controller("UnitTreeController", ["$http", function ($http) {
+    var self = this;
 
+    $http.get("http://10.10.247.136:8080/data")
+        .then(function (data) {
             var options = {
-                layout:{
+                layout: {
                     hierarchical: true
                 }
             }
@@ -18,15 +17,27 @@ app.controller("UnitTreeController", [function () {
 
             // provide the data in the vis format
             var tree = {
-                nodes: new vis.DataSet(data.nodes),
-                edges: new vis.DataSet(data.edges)
+                nodes: new vis.DataSet(data.data.nodes),
+                edges: new vis.DataSet(data.data.edges)
             }
 
             // initialize your network!
             var network = new vis.Network(container, tree, options);
-        },
-        error: function () {
 
-        }
-    });
+            network.on("selectNode", function (params) {
+                if (params.nodes.length === 1) {
+                    var node = params.nodes[0];
+
+                    $http.get("http://10.10.247.136:8080/data?id=" + node)
+                        .then(function (nodeData) {
+                            console.log(nodeData.data.name);
+                        }).catch(function () {
+
+                    });
+                }
+            });
+        })
+        .catch(function () {
+
+        })
 }]);
