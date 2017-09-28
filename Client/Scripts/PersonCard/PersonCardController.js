15d6a968e5f13e8185f968910c7f02ef6c893d00
Unknown <alon.lek@gmail.com>
2017-09-28 10:14:52 +0300

    fix bug

--- Client/Scripts/PersonCard/PersonCardController.js
+++ Client/Scripts/PersonCard/PersonCardController.js
@@ -47,10 +47,21 @@ app.controller("PersonCardController", ["appData","$timeout", function (appData,
     }
 
     self.querySearch = function(query) {
-        var results = query ? appData.data.nodes.filter( self.createFilterFor(query) ) : appData.data.nodes;
+        var results = query ? appData.data.nodes.filter( self.createFilterFor(query) ) : self.removeSelfFromCommanders(appData.data.nodes);
         return results;
     }
 
+    self.removeSelfFromCommanders = function(nodes)
+    {
+        var pos;
+        for(var elm in nodes){
+            if(nodes[elm].id == self.person.id){
+                pos = elm;
+            }
+        }
+        nodes.splice(pos, 1);
+        return nodes;
+    }
     /**
      * Create filter function for a query string
      */
@@ -58,7 +69,7 @@ app.controller("PersonCardController", ["appData","$timeout", function (appData,
         var lowercaseQuery = angular.lowercase(query);
 
         return function filterFn(commander) {
-            return (commander.label.indexOf(lowercaseQuery) === 0);
+            return (commander.label.indexOf(lowercaseQuery) === 0 && commander.id != self.person.id);
         };
     }
     
